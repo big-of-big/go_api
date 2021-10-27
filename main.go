03bac5feb8fb49c8359eba0373260786d5f19567
big-of-big <60137763+big-of-big@users.noreply.github.com>
2021-10-27 14:54:08 +0900

    first

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	_ "encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	_ "math/rand"
+	"net/http"
+	_ "strconv"
+)
+
+// モデル
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+// コントローラー
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func crateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("Hello,  world!")
+	// 初期化処理
+	r := mux.NewRouter()
+
+	// ルーティング
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", crateBook).Methods("POS")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
